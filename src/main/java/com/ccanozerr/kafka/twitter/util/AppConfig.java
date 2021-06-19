5d47ca5308b91de7922e6dcbb55649b85cef826e
ccanozerr <ccanozerr@gmail.com>
2021-06-19 14:34:33 +0300

    add: twitter client

---
+++ src/main/java/com/ccanozerr/kafka/twitter/util/AppConfig.java
@@ -0,0 +1,11 @@
+package com.ccanozerr.kafka.twitter.util;
+
+public class AppConfig {
+
+    public static final String CONSUMER_KEY = ""; // your twitter api key
+    public static final String CONSUMER_SECRET = "";// your twitter api secret key
+    public static final String TOKEN = ""; // your twitter access token
+    public static final String SECRET = "";// your twitter access token secret
+    public static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+
+}
